1f907798719b86c6b9bcb47eda66d383d014e35f
raminentezar-rgb <raminentezarprogrammer@gmail.com>
2026-04-15 10:46:28 +0300

    Fix: Set ALLOWED_HOSTS to all for production stability

--- avrasya_turnstile/settings.py
+++ avrasya_turnstile/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
